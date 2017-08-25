cd1f8a280ee8a980a2fdb3159ef64c1fe10572b0
Pragati Gupta <pragati99gupta@gmail.com>
2017-08-25 15:23:39 +0530

    Proram to write a function that returns N consecutive values in single line

---
+++ Hackerrank_Functions_ReturnValue.py
@@ -0,0 +1,14 @@
+#Printing N consecutive numbers in same line
+
+from __future__ import print_function
+if __name__ == '__main__':
+    n = int(raw_input())
+def series(n):
+    count= 1
+    for count in range(count,n+1):
+        print(count, end = '')
+        
+        
+        
+    
+series(n)
